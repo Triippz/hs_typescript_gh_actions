45c033014bb3a42243833ed79045b6f074271d0b
Mark Tripoli <mtripoli@hubspot.com>
2022-08-09 18:19:39 -0400

    Add better typing

--- project/src/app/app.functions/crm-card.ts
+++ project/src/app/app.functions/crm-card.ts
@@ -1,18 +1,18 @@
 // For external API calls
-import {ServerlessResponse} from "./types";
+import {ExternalCard, InternalCard, ServerlessAction, StaticCard} from "./types";
 
 const axios = require('axios');
 // For HubSpot API calls (HubSpot node API client)
 const hubspot = require('@hubspot/api-client');
 
-exports.main = async (context = {} as any, sendResponse: (res: ServerlessResponse) => void) => {
+exports.main = async ({context = {}, sendResponse}: ServerlessAction) => {
     // Instantiating HubSpot node API client ok
     const hubspotClient = new hubspot.Client({
         accessToken: context.secrets.PRIVATE_APP_ACCESS_TOKEN,
     });
 
     // Static CRM card example
-    const staticCard = {
+    const staticCard: StaticCard = {
         objectId: 1,
         title: 'Sample project custom CRM card',
         desc: `A custom CRM card is a UI extension that displays custom info from either HubSpot or an external system. It can also include custom actions—click the "Get Inspired" button to see example data from the ZenQuotes API.`,
@@ -29,7 +29,7 @@ exports.main = async (context = {} as any, sendResponse: (res: ServerlessRespons
                 title: 'Inspirational quotes provided by ZenQuotes API',
                 quote: data[0].q,
                 author: data[0].a,
-            };
+            } as ExternalCard;
         } catch (error: any) {
             throw new Error(
                 `There was an error fetching the quote': ${error.message}`
@@ -47,7 +47,7 @@ exports.main = async (context = {} as any, sendResponse: (res: ServerlessRespons
                 objectId: 3,
                 title: 'First contact in account',
                 firstname: resp.body.results[0].properties.firstname,
-            };
+            } as InternalCard;
         } catch (error: any) {
             throw new Error(
                 `There was an error fetching the contact': ${error.message}`
